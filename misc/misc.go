5e77afb5a7846473320eea6c91333f1caf63e5f3
Serhii Hromov <melifarowow@gmail.com>
2022-04-24 01:13:35 +0300

    miscs with get by ID

--- misc/misc.go
+++ misc/misc.go
@@ -17,6 +17,14 @@ func (m *Misc) Sources() ([]models.Source, error) {
 	return sources, nil
 }
 
+func (m *Misc) Source(ID uint64) (*models.Source, error) {
+	var source models.Source
+	if result := m.DB.First(&source, ID); result.Error != nil {
+		return nil, result.Error
+	}
+	return &source, nil
+}
+
 func (m *Misc) Users() ([]models.User, error) {
 	var users []models.User
 	if result := m.DB.Joins("Role").Find(&users); result.Error != nil {
@@ -25,10 +33,26 @@ func (m *Misc) Users() ([]models.User, error) {
 	return users, nil
 }
 
+func (m *Misc) User(ID uint64) (*models.User, error) {
+	var user models.User
+	if result := m.DB.Joins("Role").First(&user, ID); result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+
 func (m *Misc) Roles() ([]models.Role, error) {
 	var roles []models.Role
 	if result := m.DB.Find(&roles); result.Error != nil {
 		return nil, result.Error
 	}
 	return roles, nil
+}
+
+func (m *Misc) Role(ID uint64) (*models.Role, error) {
+	var role models.Role
+	if result := m.DB.First(&role, ID); result.Error != nil {
+		return nil, result.Error
+	}
+	return &role, nil
 }
